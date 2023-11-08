e9af7dae57f53bcd7780995eb3e4234fe6d73a4c
ϻartin <51332188+martin0024@users.noreply.github.com>
2023-11-07 22:55:21 -0500

    fix the arrow bug

--- support.js
+++ support.js
@@ -1,4 +1,3 @@
-
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -6,6 +5,12 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
+    var arrowDown = this.querySelector(".arrow_down");
+    var arrowUp = this.querySelector(".arrow_up");
+
+    arrowDown.classList.toggle("hidden");
+    arrowUp.classList.toggle("hidden");
+
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
